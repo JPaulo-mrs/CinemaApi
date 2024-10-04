7b5422a35aed00a3c900c66d68c391f40612b6c4
João Paulo <joaopaulodemourasjp@gmail.com>
2024-10-04 03:41:51 -0300

    Add 'post.js', implementado post na rota movies e tickets

--- K6/tests/tickets/smoke/post.js
+++ K6/tests/tickets/smoke/post.js
@@ -1,5 +1,6 @@
 import { sleep } from 'k6';
 import { group } from 'k6';
+import { SharedArray } from 'k6/data';
 import { BaseRest, BaseChecks, ENDPOINTS, testConfig } from '../../../support/base/baseTeste.js';
 
 export const options = testConfig.options.smokeTest;
@@ -9,16 +10,16 @@ const baseRest = new BaseRest(base_uri);
 const baseChecks = new BaseChecks();
 
 const data = new SharedArray('Users', function () {
-  const jsonData = JSON.parse(open('../data/dynamic/movies.json'));
-  return jsonData.movies;
+  const jsonData = JSON.parse(open('../../../data/dynamic/tickets.json'));
+  return jsonData.tickets;
 });
 
 export default () => {
   let userIndex = __ITER % data.length;
   let user = data[userIndex]
   group('Listar filmes', () => {
-    const res = baseRest.post(ENDPOINTS.MOVIES_ENDPOINT);
-    baseChecks.checkStatusCode(res, 200);
+    const res = baseRest.post(ENDPOINTS.TICKETS_ENDPOINT, user);
+    baseChecks.checkStatusCode(res, 201);
     baseChecks.checkErrorRate(res); 
     baseChecks.checkResponseTime(res);
     sleep(1);
